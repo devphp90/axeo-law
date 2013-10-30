321639a297121416010a4aed43388127ca449603
devphp90 <dev.php90@gmail.com>
2013-10-30 22:35:23 +0700

    commit first

---
+++ protected/modules/admin/views/document/view.php
@@ -0,0 +1,46 @@
+<?php
+$this->breadcrumbs=array(
+	'Documents'=>array('index'),
+	$model->name,
+);
+
+$this->menu=array(
+	array(	
+		'label'=>Yii::t('app','Create'), 
+		'icon'=>'icon-plus', 
+		'url'=>$this->createUrl('create'),
+	),
+	array(	
+		'label'=>Yii::t('app','Update'), 
+		'icon'=>'icon-pencil', 
+		'url'=>array('update','id'=>$model->id),
+	),
+	array(
+		'label'=>'Delete',
+		'icon'=>'icon-trash',
+		'url'=>'#',
+		'linkOptions'=>array(
+			'submit'=>array(
+				'delete',
+				'id'=>$model->id			),
+		'confirm'=>'Are you sure you want to delete this item?')
+	),
+	array(
+		'label'=>Yii::t('app','Manage'), 
+		'icon'=>'icon-th-list', 
+		'url'=>$this->createUrl('index')
+	),
+);
+?>
+
+<h1>View Document #<?php echo $model->id; ?></h1>
+
+<?php $this->widget('bootstrap.widgets.TbDetailView',array(
+	'data'=>$model,
+	'attributes'=>array(
+		'id',
+		'name',
+		'description',
+		'content',
+	),
+)); ?>
