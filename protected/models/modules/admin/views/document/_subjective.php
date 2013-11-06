df81a76a21c7abd7d158a1f7cfd462c36de16696
devphp90 <dev.php90@gmail.com>
2013-11-06 21:07:13 +0700

    update code from server

---
+++ protected/models/modules/admin/views/document/_subjective.php
@@ -0,0 +1,37 @@
+<?php $form=$this->beginWidget('bootstrap.widgets.TbActiveForm',array(
+	'id'=>'users-subjective-form',
+	'enableClientValidation'=>true,
+	'clientOptions'=>array(
+		'validateOnSubmit'=>true,
+	),
+)); ?>
+
+	<?php echo $form->errorSummary($model); ?>
+
+    <?php echo $form->textArea($model, 'subjective', array('class' => 'span8', 'rows' => '8' )); ?>
+
+    <?php echo $form->error($model, 'subjective'); ?>
+
+
+    <div class="compactRadioGroup">
+        <?php echo $form->radioButtonListRow($model, 'game_on_off', array(1 => 'On', 0 => 'Off')); ?>
+    </div>
+
+<div class="compactRadioGroup">
+    <?php echo $form->radioButtonListRow($model, 'serve_on_off', array(1 => 'On', 0 => 'Off')); ?>
+</div>
+
+
+<div class="compactRadioGroup">
+    <?php echo $form->radioButtonListRow($model, 'return_on_off', array(1 => 'On', 0 => 'Off')); ?>
+</div>
+
+<div class="form-actions">
+		<?php $this->widget('bootstrap.widgets.TbButton', array(
+			'buttonType'=>'submit',
+			'type'=>'primary',
+			'label'=>'Save',
+		)); ?>
+	</div>
+
+<?php $this->endWidget(); ?>
